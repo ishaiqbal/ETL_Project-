3b3d2cf42e09b62971eddedd1c5ad430093eb4ab
Whitlockg <christinw@nevada.unr.edu>
2020-02-13 20:44:51 -0500

    okay THIS is the final push

---
+++ queries.sql
@@ -0,0 +1,14 @@
+create table life_expectancy(
+year int primary key,
+country text,
+life_expectancy int,
+male_life_expectancy int,
+female_life_expectancy int
+);
+
+
+create table fertility(
+year int Primary key,
+country text,
+total_fertility_rate int
+)
